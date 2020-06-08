bc9753f93442a55378ceb82e762c1409a87d0e82
SilverImpolite <65340059+SilverImpolite@users.noreply.github.com>
2020-06-08 15:19:26 +0200

    uii

---
+++ index.py
@@ -0,0 +1,46 @@
+x = 5
+y = 2
+print(x + y)
+
+some_string = "Close your eyes!"
+
+print(some_string)
+
+string_a = "a "
+string_b = "ninja come my way"
+string_c = "what would could you say "
+
+print(string_a + string_c + "" + string_b)
+
+bool_one = True
+bool_two = False
+some_var = None
+
+# mood = input("Kako si?: ")
+
+# if mood == "bizar":
+   # print("Nadaljuj mirno a z barvo")
+#elif mood == "njah":
+    #print("Kako dolgcajt")
+#elif mood == "mijavkasto":
+   # print ("sem si mislila!")
+#else:
+    #print("whateva")
+
+#secret = 768
+
+#guess = input("Ugani stevilo: ")
+#if guess == "768":
+ #   print("Cestitke majstore!")
+#elif guess == "766":
+ #   print("Zelo blizu")
+#else:
+ #   print("Luzer... ")
+
+import random
+
+print(random.randrange(1, 10))
+
+txt = "Hello World"
+x = txt[1]
+print(x)
